d190f01ef1eba1ccf05b8c597ff7a77650805bc7
changzsean <changz.sean@gmail.com>
2026-08-31 00:36:06 +0800

    fix: use official Alibaba OAuth authorize endpoint

--- lib/alibaba/iop.ts
+++ lib/alibaba/iop.ts
@@ -32,7 +32,7 @@ export function alibabaCallbackUrl(): string {
 
 export function buildAlibabaAuthorizeUrl(state: string): string {
   const appKey = requiredEnv("ALIBABA_APP_KEY");
-  const endpoint = process.env.ALIBABA_OAUTH_AUTHORIZE_URL ?? "https://open-api.alibaba.com/oauth/authorize";
+  const endpoint = process.env.ALIBABA_OAUTH_AUTHORIZE_URL ?? "https://oauth.alibaba.com/authorize";
   const url = new URL(endpoint);
 
   url.searchParams.set("response_type", "code");
